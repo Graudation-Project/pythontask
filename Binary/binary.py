ea048fb84b6c9b6af8c8e73b1af6f75bf34bebf1
mo3az <mo3azsolomon@gmail.com>
2024-07-19 16:21:00 +0300

    project setup

---
+++ Binary/binary.py
@@ -0,0 +1,17 @@
+# Note: Validate if the number is binary or not first.
+
+def bin(number):
+    pass
+
+def dec(number):
+    
+    pass
+
+def bin_mul(bin1,bin2):
+    pass
+
+def bin_sum(bin1,bin2):
+    pass
+
+def bin_sub(bin1,bin2):
+    pass
